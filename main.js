8ec4783186f4ac83b1e32f3f30d84785b564e829
Shaziasama <shaziasamma921@gmail.com>
2024-07-18 21:33:20 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+////let 
+//let message :string = "Hello world!"
+//message = "hello i m learn typescript";  //we can assign new value we can update
+//console.log(message);
+//const
+//const pINumber: number = 12345
+//pINumber = 45676  // not update
+//console.log(pINumber);
+/////operators///////
+//Arithmitic operators( addition,multiplication, subtrction, division,modules); 
+//let add1 = 5
+//let add2 = 10
+//let addition = add1 +add2
+//console.log(addition);
+//let age = 20
+//age +=30  //when we + value so we apply += then number
+//console.log(age);
+///////subtraction///////
+//let sub1 = 10
+//let sub2 = 5
+//let subtraction = sub1-sub2
+//console.log(subtraction);
+//let sub = 12
+//sub -=10
+//console.log(sub);
+/////multiplication///////
+//const price1: number = 5
+//const price2 : number = 2
+//const multiplication = price1*price2
+//console.log(multiplication);
+//let multiply: number = 5
+//multiply  *= 2
+//console.log(multiply);
+/////// division//////
+//const amount: number = 600
+//const amount1: number = 100
+//const division = amount/amount1
+//console.log(division);
+/////// modulous//////
+//let number1 = 5
+//let number2 = 2
+//let modulus = number1 % number2
+//console.log(modulus);
+/////////Exponentiation///////
+var numb = 5;
+var numb1 = 2;
+var exponentiation = Math.pow(numb, numb1);
+console.log(exponentiation);
